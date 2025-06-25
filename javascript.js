c0aab3335af6c0a8e1d1d4a237995fc47cd59932
1256639 <167916944+1256639@users.noreply.github.com>
2025-06-25 15:26:51 -0400

    Layout completed Digit input functions along with clear all and clear one

--- javascript.js
+++ javascript.js
@@ -33,3 +33,56 @@ function operate(operator, a, b){
 let firstNumber = null;
 let operator = null;
 let secondNumber = null;
+
+let displayValue = "0";
+
+const display = document.getElementById("display");
+const digitButtons = document.querySelectorAll(".digit");
+
+// Updates display
+function updateDisplay(){
+    display.textContent = displayValue;
+}
+
+// Button clicks
+function appendDigit(digit){
+    if (displayValue === "0"){
+        displayValue = digit;
+    } else {
+        displayValue += digit;
+    }
+    updateDisplay();
+}
+
+// Click listener for all buttons
+function handleDigitClick(event) {
+    const digit = event.target.getAttribute("data-digit");
+    appendDigit(digit);
+}
+
+digitButtons.forEach(function(btn) {
+    btn.addEventListener("click", handleDigitClick);
+});
+
+// Clear all
+function handleAllClearClick() {
+    displayValue = "0";
+    updateDisplay();
+}
+
+document.getElementById("all-clear").addEventListener("click", handleAllClearClick);
+
+// Backspace
+function handleClearClick() {
+    if (displayValue.length > 1) {
+        displayValue = displayValue.slice(0, -1);
+    } else {
+        displayValue = "0";
+    }
+    updateDisplay();
+}
+
+document.getElementById("clear").addEventListener("click", handleClearClick);
+
+
+updateDisplay();
